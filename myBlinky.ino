35309a0c9e90668052abc9644f77f906ab57949c
My Name <me@mybox.com>
2018-11-22 11:09:44 -0500

    It works! First blink.

--- myBlinky.ino
+++ myBlinky.ino
@@ -1,6 +1,13 @@
+// Simple Demo Blinker -MEH
+#define PIN_LED 13
 
 void setup() {
+	pinMode(PIN_LED,OUTPUT);
 }
 
 void loop() {
+	digitalWrite(PIN_LED,HIGH);
+	delay(500);
+	digitalWrite(PIN_LED,LOW);
+	delay(500);
 }
