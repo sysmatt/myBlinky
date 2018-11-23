122911f99dddce2dabbb251c3b640c8c7f9f98d9
My Name <me@mybox.com>
2018-11-22 21:22:59 -0500

    Blinky with NeoPixels

--- myBlinky.ino
+++ myBlinky.ino
@@ -1,13 +1,20 @@
-// Simple Demo Blinker -MEH
-#define PIN_LED 2
+// Fancy NeoPixel Blinky Blinker
+
+#include <Adafruit_NeoPixel.h>
+#define PIN_LED 14
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(1, PIN_LED, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-	pinMode(PIN_LED,OUTPUT);
+	strip.begin();
 }
 
 void loop() {
-	digitalWrite(PIN_LED,HIGH);
-	delay(500);
-	digitalWrite(PIN_LED,LOW);
-	delay(500);
+	
+	strip.setPixelColor(0,strip.Color(255,0,0)); strip.show();
+	delay(200);
+	strip.setPixelColor(0,strip.Color(0,255,0)); strip.show();
+	delay(200);
+	strip.setPixelColor(0,strip.Color(0,0,255)); strip.show();
+	delay(200);
 }
